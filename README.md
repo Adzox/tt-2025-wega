4d6167f70ccd52c5d9d8f94f86dc716acf8ace19
Kevin MV <somjai.kevin@gmail.com>
2025-03-08 15:55:37 +0900

    Add basic project setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tech Test 2025 - WEGA
+
+This repository contains the answers to the tech test - please enjoy!
+
+## Requirements
+
+Write your code in TypeScript and use pixi.js (v7) for rendering.
+
+* Each task should be accessed via an in-game menu.
+* Render responsively for both mobile and desktop devices.
+* Display the fps in the top left corner.
+* Run the application in full screen.
---
+++ src/game.ts
@@ -0,0 +1,18 @@
+import { Application, Assets, BaseTexture, SCALE_MODES, Sprite, Texture } from 'pixi.js';
+
+import TestAsset from "./assets/test.png";
+
+export async function initGame() {
+    const app = new Application<HTMLCanvasElement>({ width: 640, height: 320, resizeTo: document.body });
+    app.view.style.imageRendering = 'pixelated';
+    document.body.appendChild(app.view);
+
+    BaseTexture.defaultOptions.scaleMode = SCALE_MODES.NEAREST;
+
+    const texture = await Assets.load(TestAsset) as Texture;
+    const a = new Sprite(texture);
+    a.x = app.renderer.width / 2;
+    a.y = app.renderer.height / 2;
+
+    app.stage.addChild(a);
+}
